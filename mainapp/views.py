e4c67aa760dc9151ce44b8f73eaddb8094473413
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-01-25 10:53:47 +0530

    Implement Search and category query

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,6 +8,8 @@ from rest_framework import status
 from . models import Image,Product
 from . serializers import ProductSerializer,ImageSerializer
 import requests
+from .models import Product,Image
+
 
 class ProductList(APIView):
 
@@ -15,8 +17,27 @@ class ProductList(APIView):
 		search = request.GET.get('search')
 		category = request.GET.get('cat')
 		sort = request.GET.get('sort')
-		print("YAASS",search,category,sort)
-		product_list = Product.objects.all()
+		# print("YAASS",search,category,sort)
+		if(category and category!='All'):
+			cat_filter=Product.objects.filter(category='Electronics')
+		else:
+			cat_filter=Product.objects.all()
+		if(search):
+			temp1=cat_filter.filter(productname__icontains=search)
+			temp2=cat_filter.filter(description__icontains=search)
+			search_filter=temp1
+			search_filter=search_filter.union(temp2)
+			for word in search.split():
+				temp1=cat_filter.filter(productname__icontains=word)
+				temp2=cat_filter.filter(description__icontains=word)
+				search_filter=search_filter.union(temp1)
+				search_filter=search_filter.union(temp2)
+		else:
+			search_filter=cat_filter
+			
+
+		print(search_filter)
+		product_list = search_filter
 		serializer = ProductSerializer(product_list,many = True)
 
 		return Response(serializer.data)
@@ -60,8 +81,6 @@ def store(request):
 	# print(search,category,sort)
 	param={'search':search,'cat':category,"sort":sort}
 	getdata = requests.get('http://127.0.0.1:8000/mainapp/productdatabase/',params=param)
-
-	# listdata=
 
 	return render(request = request,
 								template_name = "mainapp/store.html")
