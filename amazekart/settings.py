39528e382cd4cea03f9d7f5aa92f6330998cef0e
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-02-17 21:26:51 +0530

    add ip

--- amazekart/settings.py
+++ amazekart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!#ex+7286g+le3u8!fmynv+9@31juw2yj)qqf13bygw(2#a-lv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.3.9.174','10.3.9.199','10.3.9.49','10.3.9.62','localhost','127.0.0.1','18.222.65.138','ec2-18-222-65-138.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['*']
 
 # APPEND_SLASH=False
 # Application definition
