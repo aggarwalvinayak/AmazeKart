04d674f874216493ca4bc3824060f9fbc302c945
Rohan Prabhu <31131133+RohanPK@users.noreply.github.com>
2020-02-19 14:04:21 +0530

    Update settings.py

--- amazekart/settings.py
+++ amazekart/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 CSRF_COOKIE_SECURE=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
